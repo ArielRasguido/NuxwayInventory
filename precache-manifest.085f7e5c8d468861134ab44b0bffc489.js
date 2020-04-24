9a89a658af261dda7bac7bb7bdc198bd66a6f5fc
ariel <ariel_arc.95@hotmail.com>
2020-04-23 23:13:08 -0400

    Updates

---
+++ precache-manifest.085f7e5c8d468861134ab44b0bffc489.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ee3ceeb5d1c8a62a0793796a2bd51d",
+    "url": "/NuxwayInventory/index.html"
+  },
+  {
+    "revision": "7874c1c0093cc3ba3a50",
+    "url": "/NuxwayInventory/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "21175ae407c9ca139959",
+    "url": "/NuxwayInventory/static/css/main.cba0dcc0.chunk.css"
+  },
+  {
+    "revision": "7874c1c0093cc3ba3a50",
+    "url": "/NuxwayInventory/static/js/2.48be2b6b.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/NuxwayInventory/static/js/2.48be2b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21175ae407c9ca139959",
+    "url": "/NuxwayInventory/static/js/main.1e4289a2.chunk.js"
+  },
+  {
+    "revision": "b2a4dc65eab333faecb6",
+    "url": "/NuxwayInventory/static/js/runtime-main.05eff1cc.js"
+  },
+  {
+    "revision": "b4bf4147b7e3c7587ae3f983adffe017",
+    "url": "/NuxwayInventory/static/media/clientes.b4bf4147.png"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/NuxwayInventory/static/media/delete.650cdc57.svg"
+  },
+  {
+    "revision": "7f4f12d49bcbaa692898c0d04d32c050",
+    "url": "/NuxwayInventory/static/media/edit.7f4f12d4.svg"
+  },
+  {
+    "revision": "e2af00bf70de49b679c24c7d685dbb66",
+    "url": "/NuxwayInventory/static/media/logonuxway.e2af00bf.png"
+  },
+  {
+    "revision": "b3703b49ae4296ae7e5b475ad41b02cc",
+    "url": "/NuxwayInventory/static/media/logonuxway2.b3703b49.png"
+  },
+  {
+    "revision": "97386ce98ec442c1688c921c52fb7215",
+    "url": "/NuxwayInventory/static/media/logonuxway3.97386ce9.png"
+  },
+  {
+    "revision": "cb923278b945ab639bc35b41d3952587",
+    "url": "/NuxwayInventory/static/media/productos.cb923278.png"
+  }
+]);
